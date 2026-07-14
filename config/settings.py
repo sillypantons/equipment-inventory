1b622df211c849740a3be0c65ef71ca333750a47
alexcampbell <alex.campbell@pantonmcleod.co.uk>
2026-07-14 10:38:02 +0100

    Fix CSRF trusted origins

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-AH4V^9zN+$Dj"C2A6WA?£L(|FX+`OR92k~NAjaJ{qvZ<ZW%oT-')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+CSRF_TRUSTED_ORIGINS = [
+    'https://equipment-inventory-production-49ee.up.railway.app',  # replace with your actual Railway URL
+]
 
 
 
