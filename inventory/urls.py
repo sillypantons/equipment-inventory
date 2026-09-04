d0473f84bab543029b7e309903c1cc1e40d48c72
alexcampbell <alex.campbell@pantonmcleod.co.uk>
2026-09-04 09:19:37 +0100

    Add sign in out URL

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,10 +6,12 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.equipment_list, name='equipment_list'),
     path('equipment/<str:SAGE_num>/', views.equipment_detail, name='equipment_detail'),
+    path('equipment/<str:SAGE_num>/edit/', views.edit_equipment, name='edit_equipment'),
+    path('equipment/<str:SAGE_num>/sign-in-out/', views.sign_in_out, name='sign_in_out'),
     path('dashboard/', views.request_dashboard, name='request_dashboard'),
     path('dashboard/update/<int:request_id>/', views.update_request_status, name='update_request_status'),
     path('dashboard/delete/<int:request_id>/', views.delete_request, name='delete_request'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('equipment/<str:SAGE_num>/edit/', views.edit_equipment, name='edit_equipment'),
+
 ]
